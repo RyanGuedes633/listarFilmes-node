a5935718a86f93f980fe3999142551d08a495f8a
JVSantos7 <jussandro_vitor@hotmail.com>
2026-05-25 17:58:24 -0300

    fix:correcao das imagens dos cards

--- vue-app/src/components/MovieCard.vue
+++ vue-app/src/components/MovieCard.vue
@@ -1,17 +1,17 @@
 <template>
     <article class="movie-card">
-    <img v-if="movie.poster_path" :src="movie.poster_path" :alt="movie.name || movie.title"/>
+    <img v-if="posterUrl" :src="posterUrl" :alt="props.movie.name || props.movie.title"/>
 
     <div>
         
-        <h3>{{ movie.name || movie.title }}</h3>
+        <h3>{{ props.movie.name || props.movie.title }}</h3>
         
-        <p v-if="movie.first_air_date">
-        Ano: {{ movie.first_air_date.slice(0, 4) }}
+        <p v-if="props.movie.first_air_date">
+        Ano: {{ props.movie.first_air_date.slice(0, 4) }}
         </p>
         
-        <p v-if="movie.vote_average">
-        Nota: {{ movie.vote_average.toFixed(1) }}
+        <p v-if="props.movie.vote_average">
+        Nota: {{ props.movie.vote_average.toFixed(1) }}
         </p>
 
         <p>
@@ -39,6 +39,8 @@ const props = defineProps({
 defineEmits(['favorite'])
 
 const posterUrl = computed(() => {
-  return `https://image.tmdb.org/t/p/w342${props.movie.poster_path}`
+    if (!props.movie.poster_path) return ''
+    return `https://image.tmdb.org/t/p/w300${props.movie.poster_path}`
 })
+console.log(props.movie.poster_path) 
 </script>
